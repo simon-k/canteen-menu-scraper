ace095bd7ab084d7c4e5d778e9a9540426582975
Simon Kofodm <skfd@novonordisk.com>
2025-03-27 13:53:28 +0100

    Use AI to parse Hub1 menu

--- CanteenParser/HubNordicMenuParser.cs
+++ CanteenParser/HubNordicMenuParser.cs
@@ -60,12 +60,17 @@ public class HubNordicMenuParser
     private static DateTimeOffset GetDateFromWeekday(string weekday)
     {
         var dayOfWeek = WeekdayToDayOfWeek(weekday);
+        return GetDateFromWeekday(dayOfWeek);
+    }
+
+    private static DateTimeOffset GetDateFromWeekday(DayOfWeek dayOfWeek)
+    {
         var todaysDate = DateTime.Today;
         var daysUntil = ((int)dayOfWeek - (int)todaysDate.DayOfWeek);
         var date = todaysDate.AddDays(daysUntil);
         return date;
     }
-    
+
     private static DayOfWeek WeekdayToDayOfWeek(string weekday)
     {
         return weekday switch
@@ -79,5 +84,47 @@ public class HubNordicMenuParser
             "Søndag" => DayOfWeek.Sunday,
             _ => throw new ArgumentException("Weekday not found")
         };
+    }
+
+    public static List<Dish> GetAllDishes(Hub1Menu menu)
+    {
+        var dishes = new List<Dish>();
+
+        //Kays
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Monday),    Kind = KindParser.GetKind(menu.Kays.Monday.MainDish),   Name = "Kays", Description = menu.Kays.Monday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Tuesday),   Kind = KindParser.GetKind(menu.Kays.Tuesday.MainDish),  Name = "Kays", Description = menu.Kays.Tuesday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Wednesday), Kind = "Vegetarian",                                    Name = "Kays", Description = menu.Kays.Wednesday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Thursday),  Kind = KindParser.GetKind(menu.Kays.Thursday.MainDish), Name = "Kays", Description = menu.Kays.Thursday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Friday),    Kind = KindParser.GetKind(menu.Kays.Friday.MainDish),   Name = "Kays", Description = menu.Kays.Friday.MainDish });
+        
+        //Kays - Vegetarian
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Monday),    Kind = "Vegetarian", Name = "Kays", Description = menu.Kays.Monday.Vegetarian });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Tuesday),   Kind = "Vegetarian", Name = "Kays", Description = menu.Kays.Tuesday.Vegetarian });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Wednesday), Kind = "Vegetarian", Name = "Kays", Description = menu.Kays.Wednesday.Vegetarian });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Thursday),  Kind = "Vegetarian", Name = "Kays", Description = menu.Kays.Thursday.Vegetarian });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Friday),    Kind = "Vegetarian", Name = "Kays", Description = menu.Kays.Friday.Vegetarian });
+        
+        //Homebound
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Monday),    Kind = KindParser.GetKind(menu.Homebound.Monday.MainDish),   Name = "Homebound", Description = menu.Homebound.Monday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Tuesday),   Kind = KindParser.GetKind(menu.Homebound.Tuesday.MainDish),  Name = "Homebound", Description = menu.Homebound.Tuesday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Wednesday), Kind = "Vegetarian",                                         Name = "Homebound", Description = menu.Homebound.Wednesday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Thursday),  Kind = KindParser.GetKind(menu.Homebound.Thursday.MainDish), Name = "Homebound", Description = menu.Homebound.Thursday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Friday),    Kind = KindParser.GetKind(menu.Homebound.Friday.MainDish),   Name = "Homebound", Description = menu.Homebound.Friday.MainDish });
+        
+        //Globetrotter
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Monday),    Kind = KindParser.GetKind(menu.Globetrotter.Monday.MainDish),   Name = "Globetrotter", Description = menu.Globetrotter.Monday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Tuesday),   Kind = KindParser.GetKind(menu.Globetrotter.Tuesday.MainDish),  Name = "Globetrotter", Description = menu.Globetrotter.Tuesday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Wednesday), Kind = "Vegetarian",                                            Name = "Globetrotter", Description = menu.Globetrotter.Wednesday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Thursday),  Kind = KindParser.GetKind(menu.Globetrotter.Thursday.MainDish), Name = "Globetrotter", Description = menu.Globetrotter.Thursday.MainDish });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Friday),    Kind = KindParser.GetKind(menu.Globetrotter.Friday.MainDish),   Name = "Globetrotter", Description = menu.Globetrotter.Friday.MainDish });
+        
+        //Sprout
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Monday),    Kind = "Vegetarian", Name = "Sprout", Description = "Salatbar" });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Tuesday),   Kind = "Vegetarian", Name = "Sprout", Description = "Salatbar" });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Wednesday), Kind = "Vegetarian", Name = "Sprout", Description = "Salatbar" });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Thursday),  Kind = "Vegetarian", Name = "Sprout", Description = "Salatbar" });
+        dishes.Add(new Dish { Date = GetDateFromWeekday(DayOfWeek.Friday),    Kind = "Vegetarian", Name = "Sprout", Description = "Salatbar" });
+        
+        return dishes;
     }
 }
--- CanteenParser/Program.cs
+++ CanteenParser/Program.cs
@@ -6,17 +6,21 @@ using CanteenParser.Writers;
 var canteenId = args[0];
 var username = args[1];     //TODO: Make user and password optional
 var password = args[2];
-var gistToken = args[3];
-var gistId = args[4];
-var gistFilename = args[5];
+var aiKey = args[3];
+var gistToken = args[4];
+var gistId = args[5];
+var gistFilename = args[6];
 
 List<Dish> dishes;
 
 if (canteenId == "Hub1")
 {
-    var websiteReader = new HubNordicWebsiteReader();
+    /*var websiteReader = new HubNordicWebsiteReader();
     var websiteContent = websiteReader.ReadWebsiteContentAsync();
-    dishes = HubNordicMenuParser.GetAllDishes(websiteContent);
+    dishes = HubNordicMenuParser.GetAllDishes(websiteContent);*/
+    var menuReader = new HubNordicAiReader();
+    var menu = await menuReader.ReadAsync(aiKey);
+    dishes = HubNordicMenuParser.GetAllDishes(menu);
 }
 else
 {
